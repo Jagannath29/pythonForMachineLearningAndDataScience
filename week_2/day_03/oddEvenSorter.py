1afb843805141457a0296bbeab15ec180ac0214c
Jagannath29 <khadkaj84@gmail.com>
2024-02-27 19:17:45 +0545

    odd even sorter

--- week_2/day_03/oddEvenSorter.py
+++ week_2/day_03/oddEvenSorter.py
@@ -14,3 +14,26 @@
 
 # 1. Allow the user to input any type of values (not just numbers) and handle different data types.
 # 2. Enhance the program to display the sorted lists in ascending or descending order.
+
+def oddEvenSorter(number_list):
+    even_list = [even for even in number_list if (even % 2 == 0)]
+    odd_list = [odd for odd in number_list if (odd % 2 != 0)]
+    
+    # for index in number_list:
+    #     if index % 2 == 0:
+    #         even_list.append(index)
+    #     elif index % 2 != 0:
+    #         odd_list.append(index)
+    #     else:
+    #         print('invalid input')
+    
+    
+    return odd_list, even_list
+
+my_list = [1, 2, 3, 4, 5, 6]
+# my_list = list(map(int, input())) did not work
+# for i in range(my_list):
+#     inp = int(input("Enter a list of a number: "))
+#     my_list.append(inp)
+print(oddEvenSorter(my_list))
+  
