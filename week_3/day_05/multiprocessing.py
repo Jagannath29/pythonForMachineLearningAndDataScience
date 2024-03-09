0e69b8f87b9bce7d64489d5eaa16992b792e43b5
Jagannath29 <khadkaj84@gmail.com>
2024-03-09 21:20:18 +0545

    MultiProcessing and asynchronization

---
+++ week_3/day_05/multiprocessing.py
@@ -0,0 +1,25 @@
+import requests
+from program_timer import timer
+from multiprocessing.pool import Pool
+
+
+url = 'https://httpbin.org/uuid'
+
+def fetch_uuid(session, url):
+    with session.get(url) as response:
+        print(response.json()['uuid'])
+        
+# @timer(1,1)
+def main():
+    with Pool() as pool:
+        with requests.Session() as session:
+            pool.starmap(fetch_uuid, [(session, url) for _ in range(100)])
+            
+            
+if __name__ == "__main__":
+    main()
+
+
+
+
+
